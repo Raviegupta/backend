d735f707736ccffdc1b2c19c54a9744d3a1cb1e9
Raviegupta <raviegupta153@gmail.com>
2024-05-08 09:21:03 +0530

    controller and router setup

--- src/app.js
+++ src/app.js
@@ -15,4 +15,15 @@ app.use(express.urlencoded({extended: true, limit: "16kb"}))
 app.use(express.static("public"))
 app.use(cookieParser())
 
+
+// routes 
+import userRouter from "./routes/user.routes.js"    // (desired name can be given only when the export is being done by default.)
+
+
+// routes declaration
+app.use("/api/v1/users", userRouter)
+
+
+// http://localhost:8000/api/v1/users/register
+// http://localhost:8000/api/v1/users/login
 export { app }
